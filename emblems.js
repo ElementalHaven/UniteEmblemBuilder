689196095c014888ab3024e9c480b06d906c1d58
ElementalHaven <84058938+ElementalHaven@users.noreply.github.com>
2022-07-23 03:11:05 -0400

    tab system and emblem table

--- emblems.js
+++ emblems.js
@@ -77,7 +77,13 @@ const combos = [
 ];
 var activeEmblems;
 var userEmblems;
+var defaultTier = 0;
+const tierNames = ["Bronze", "Silver", "Gold"];
 const maxEmblems = 10;
+const statNames = [
+    "HP", "Attack", "Defense", "Sp. Attack", "Sp. Defense",
+    "Speed", "Critical-Hit Rate"
+];
 //#endregion
 function getComboEffect(color, count) {
     for (const combo of combos) {
@@ -99,10 +105,10 @@ function getComboEffect(color, count) {
         return effect;
     }
 }
-function applyFlatEffect(effect, effects) {
+function applyFlatEffect(effect, effects, multiplier = 1) {
     var _a;
     let val = (_a = effects.get(effect.stat)) !== null && _a !== void 0 ? _a : 0;
-    effects.set(effect.stat, val + effect.amount);
+    effects.set(effect.stat, val + effect.amount * multiplier);
 }
 function calculateResults() {
     var _a;
@@ -112,8 +118,8 @@ function calculateResults() {
         let emblem = activeEmblems[i];
         let pokemon = pkmnByName.get(emblem.pokemonName);
         let grade = pokemon.grades[emblem.grade];
-        applyFlatEffect(grade.posEffect, effects);
-        applyFlatEffect(grade.negEffect, effects);
+        applyFlatEffect(grade.posEffect, effects, emblem.count);
+        applyFlatEffect(grade.negEffect, effects, emblem.count);
         let applyColors = true;
         for (let j = 0; j < i; j++) {
             if (activeEmblems[j].pokemonName == emblem.pokemonName) {
@@ -157,49 +163,83 @@ function nidoranCheck(line) {
     }
     return "Nidoran♂";
 }
-function emblemsFromText(lines) {
-    let emblems = [];
-    for (let line of lines) {
-        line = line.trim().toLowerCase();
-        if (!line.length)
-            continue;
-        let parts = line.split(/[ \t]+/);
-        let tier = 0;
-        if (parts.includes("bronze"))
-            tier = 0;
-        else if (parts.includes("silver"))
-            tier = 1;
-        else if (parts.includes("gold"))
-            tier = 2;
-        let name = nidoranCheck(line);
-        if (!name) {
-            for (let possibleName of pkmnByName.keys()) {
-                let lowerName = possibleName.toLowerCase();
-                if (parts.includes(lowerName)) {
-                    name = possibleName;
-                    break;
-                }
+function parseEmblem(line) {
+    line = line.trim().toLowerCase();
+    if (!line.length)
+        return null;
+    let parts = line.split(/[ \t]+/);
+    let tier = defaultTier;
+    // TODO add emoji support
+    if (parts.includes("bronze"))
+        tier = 0;
+    else if (parts.includes("silver"))
+        tier = 1;
+    else if (parts.includes("gold"))
+        tier = 2;
+    let name = nidoranCheck(line);
+    if (!name) {
+        for (let possibleName of pkmnByName.keys()) {
+            let lowerName = possibleName.toLowerCase();
+            if (parts.includes(lowerName)) {
+                name = possibleName;
+                break;
             }
         }
-        if (name) {
-            emblems.push({
-                pokemonName: name,
-                grade: tier
-            });
-            if (emblems.length >= 10)
-                break;
+    }
+    let count = 1;
+    for (let part of parts) {
+        let parseAttempt = NaN;
+        if (part.startsWith('x')) {
+            parseAttempt = parseInt(part.substring(1));
         }
+        else if (part.endsWith('x')) {
+            parseAttempt = parseInt(part.substring(0, part.length - 1));
+        }
+        else {
+            parseAttempt = parseInt(part);
+        }
+        // already covers NaN
+        if (parseAttempt > 0) {
+            count = parseAttempt;
+            break;
+        }
+    }
+    let emblem = null;
+    if (name) {
+        emblem = {
+            pokemonName: name,
+            grade: tier,
+            count: count
+        };
+    }
+    return emblem;
+}
+function emblemsFromText(lines, limit) {
+    limit !== null && limit !== void 0 ? limit : (limit = 2970); // 3 tiers * 99 pokemon * 10 slots
+    let emblems = [];
+    let count = 0;
+    for (let line of lines) {
+        let emblem = parseEmblem(line);
+        if (!emblem)
+            continue;
+        if (emblem.count + count > limit) {
+            emblem.count = limit - count;
+        }
+        emblems.push(emblem);
+        count += emblem.count;
+        if (count >= limit)
+            break;
     }
     return emblems;
 }
 function createFromTextfield(input) {
-    let output = document.getElementById("test-output");
+    let output = document.querySelector("[data-tab='free'] .output");
     // remove old results
     while (output.firstElementChild) {
         output.firstElementChild.remove();
     }
     let lines = input.value.split('\n');
-    activeEmblems = emblemsFromText(lines);
+    activeEmblems = emblemsFromText(lines, 10);
     const effects = calculateResults();
     // add new results
     for (let effect of effects) {
@@ -211,16 +251,90 @@ function createFromTextfield(input) {
         output.append(tag);
     }
 }
+function setActiveTab(tab, updateHash) {
+    var _a, _b, _c, _d;
+    (_b = (_a = document.querySelector(".tab-list > .active")) === null || _a === void 0 ? void 0 : _a.classList) === null || _b === void 0 ? void 0 : _b.remove("active");
+    (_d = (_c = document.querySelector(".tab-content > .active")) === null || _c === void 0 ? void 0 : _c.classList) === null || _d === void 0 ? void 0 : _d.remove("active");
+    document.querySelectorAll(`[data-tab="${tab}"]`).forEach(t => t.classList.add("active"));
+    if (updateHash)
+        document.location.hash = tab;
+}
+function getStatValue(stat, grade) {
+    var _a, _b;
+    if (((_a = grade.negEffect) === null || _a === void 0 ? void 0 : _a.stat) === stat)
+        return grade.negEffect.amount;
+    if (((_b = grade.posEffect) === null || _b === void 0 ? void 0 : _b.stat) === stat)
+        return grade.posEffect.amount;
+    return 0;
+}
+// created for the whole minute the stat list was generated dynamically
+// that code was removed to control the order of the stat list
+function addUnique(obj, array) {
+    if (obj != null && !array.includes(obj))
+        array.push(obj);
+}
+function createTH(text) {
+    let cell = document.createElement("th");
+    cell.innerText = text;
+    return cell;
+}
+function setupInfoTable() {
+    let tbl = document.querySelector("[data-tab='info'] table");
+    // set up header
+    for (let stat of statNames) {
+        // FIXME also need sorting buttons
+        tbl.tHead.rows[0].append(createTH(stat));
+    }
+    tbl.tHead.rows[0].append(createTH("Color 1"));
+    tbl.tHead.rows[0].append(createTH("Color 2"));
+    tbl.tHead.rows[0].append(createTH("Owned"));
+    // rows for each pokemon
+    for (let pokemon of pkmnList) {
+        for (let tier in pokemon.grades) {
+            let grade = pokemon.grades[tier];
+            let row = tbl.tBodies[0].insertRow();
+            // name & grade
+            let cell = row.insertCell();
+            cell.innerText = pokemon.name;
+            cell.classList.add(tierNames[tier]);
+            // stats
+            for (let stat of statNames) {
+                cell = row.insertCell();
+                let val = getStatValue(stat, grade);
+                if (val) {
+                    let dec = 0;
+                    if (stat.includes("Attack") || stat.includes("Critical")) {
+                        dec = 1;
+                    }
+                    cell.innerText = val.toFixed(dec);
+                    cell.classList.add("stat");
+                    cell.classList.add(val > 0 ? "good" : "bad");
+                }
+            }
+            // colors
+            row.insertCell().innerText = pokemon.colors[0];
+            cell = row.insertCell();
+            if (pokemon.colors.length > 1)
+                cell.innerText = pokemon.colors[1];
+            // owned
+            row.insertCell();
+        }
+    }
+}
 function setup() {
-    let input = document.getElementById("test-input");
+    // need falsy rather than nullsy
+    setActiveTab((document.location.hash || "#free").substring(1), false);
+    let input = document.querySelector("[data-tab='free'] textarea");
     input.addEventListener("keydown", () => createFromTextfield(input));
     input.addEventListener("keyup", () => createFromTextfield(input));
     input.addEventListener("paste", () => createFromTextfield(input));
+    document.querySelectorAll(".tab-list > *").forEach(t => t.addEventListener("click", ev => setActiveTab(t.dataset.tab, true)));
     fetch("emblems.json").then(r => r.json()).then(json => {
         pkmnList = json;
         for (let pkmn of pkmnList) {
             pkmnByName.set(pkmn.name, pkmn);
         }
+        setupInfoTable();
         createFromTextfield(input);
     });
 }
--- emblems.ts
+++ emblems.ts
@@ -29,6 +29,7 @@ class Grade {
 class Emblem {
 	pokemonName: string;
 	grade: number;
+	count: number;
 }
 
 //#endregion
@@ -95,7 +96,13 @@ const combos: ComboEffect[] = [
 ]
 var activeEmblems: Emblem[];
 var userEmblems: Emblem[];
+var defaultTier = 0;
+const tierNames = ["Bronze", "Silver", "Gold"];
 const maxEmblems = 10;
+const statNames = [
+	"HP", "Attack", "Defense", "Sp. Attack", "Sp. Defense",
+	"Speed", "Critical-Hit Rate"
+];
 
 //#endregion
 
@@ -116,9 +123,13 @@ function getComboEffect(color: string, count: number): ResultingEffect {
 	}
 }
 
-function applyFlatEffect(effect: Effect, effects: Map<string, number>): void {
+function applyFlatEffect(
+	effect: Effect,
+	effects: Map<string, number>,
+	multiplier: number = 1
+): void {
 	let val = effects.get(effect.stat) ?? 0;
-	effects.set(effect.stat, val + effect.amount);
+	effects.set(effect.stat, val + effect.amount * multiplier);
 }
 
 function calculateResults(): ResultingEffect[] {
@@ -129,8 +140,8 @@ function calculateResults(): ResultingEffect[] {
 		let pokemon = pkmnByName.get(emblem.pokemonName);
 		let grade = pokemon.grades[emblem.grade];
 
-		applyFlatEffect(grade.posEffect, effects);
-		applyFlatEffect(grade.negEffect, effects);
+		applyFlatEffect(grade.posEffect, effects, emblem.count);
+		applyFlatEffect(grade.negEffect, effects, emblem.count);
 
 		let applyColors = true;
 		for(let j = 0; j < i; j++) {
@@ -177,49 +188,85 @@ function nidoranCheck(line: string) {
 	return "Nidoran♂";
 }
 
-function emblemsFromText(lines: string[]): Emblem[] {
-	let emblems: Emblem[] = [];
+function parseEmblem(line: string): Emblem {
+	line = line.trim().toLowerCase();
+	if(!line.length) return null;
 
-	for(let line of lines) {
-		line = line.trim().toLowerCase();
-		if(!line.length) continue;
-		let parts = line.split(/[ \t]+/);
+	let parts = line.split(/[ \t]+/);
 
-		let tier = 0;
-		if(parts.includes("bronze")) tier = 0;
-		else if(parts.includes("silver")) tier = 1;
-		else if(parts.includes("gold")) tier = 2;
+	let tier = defaultTier;
+	// TODO add emoji support
+	if(parts.includes("bronze")) tier = 0;
+	else if(parts.includes("silver")) tier = 1;
+	else if(parts.includes("gold")) tier = 2;
 
-		let name = nidoranCheck(line);
-		if(!name) {
-			for(let possibleName of pkmnByName.keys()) {
-				let lowerName = possibleName.toLowerCase();
-				if(parts.includes(lowerName)) {
-					name = possibleName;
-					break;
-				}
+	let name = nidoranCheck(line);
+	if(!name) {
+		for(let possibleName of pkmnByName.keys()) {
+			let lowerName = possibleName.toLowerCase();
+			if(parts.includes(lowerName)) {
+				name = possibleName;
+				break;
 			}
 		}
-		if(name) {
-			emblems.push({
-				pokemonName: name,
-				grade: tier
-			});
-			if(emblems.length >= 10) break;
+	}
+
+	let count = 1;
+	for(let part of parts) {
+		let parseAttempt = NaN;
+		if(part.startsWith('x')) {
+			parseAttempt = parseInt(part.substring(1));
+		} else if(part.endsWith('x')) {
+			parseAttempt = parseInt(part.substring(0, part.length - 1));
+		} else {
+			parseAttempt = parseInt(part);
 		}
+
+		// already covers NaN
+		if(parseAttempt > 0) {
+			count = parseAttempt;
+			break;
+		}
+	}
+
+	let emblem = null;
+	if(name) {
+		emblem = {
+			pokemonName: name,
+			grade: tier,
+			count: count
+		};
+	}
+	return emblem;
+}
+
+function emblemsFromText(lines: string[], limit?: number): Emblem[] {
+	limit ??= 2970; // 3 tiers * 99 pokemon * 10 slots
+	let emblems: Emblem[] = [];
+	let count = 0;
+	for(let line of lines) {
+		let emblem = parseEmblem(line);
+		if(!emblem) continue;
+
+		if(emblem.count + count > limit) {
+			emblem.count = limit - count;
+		}
+		emblems.push(emblem);
+		count += emblem.count;
+		if(count >= limit) break;
 	}
 	return emblems;
 }
 
 function createFromTextfield(input: HTMLTextAreaElement) {
-	let output = document.getElementById("test-output");
+	let output = document.querySelector("[data-tab='free'] .output");
 	// remove old results
 	while(output.firstElementChild) {
 		output.firstElementChild.remove()
 	}
 
 	let lines = input.value.split('\n');
-	activeEmblems = emblemsFromText(lines);
+	activeEmblems = emblemsFromText(lines, 10);
 
 	const effects = calculateResults();
 
@@ -234,18 +281,105 @@ function createFromTextfield(input: HTMLTextAreaElement) {
 	}
 }
 
-function setup(): void {
+function setActiveTab(tab: string, updateHash: boolean): void {
+	document.querySelector(".tab-list > .active")?.classList?.remove("active");
+	document.querySelector(".tab-content > .active")?.classList?.remove("active");
+	document.querySelectorAll(`[data-tab="${tab}"]`).forEach(
+		t => t.classList.add("active")
+	);
+	if(updateHash) document.location.hash = tab;
+}
 
-	let input = document.getElementById("test-input") as HTMLTextAreaElement;
+function getStatValue(stat: string, grade: Grade): number {
+	if(grade.negEffect?.stat === stat) return grade.negEffect.amount;
+	if(grade.posEffect?.stat === stat) return grade.posEffect.amount;
+	return 0;
+}
+
+// created for the whole minute the stat list was generated dynamically
+// that code was removed to control the order of the stat list
+function addUnique<T>(obj: T, array: T[]) {
+	if(obj != null && !array.includes(obj)) array.push(obj);
+}
+
+function createTH(text: string): HTMLTableHeaderCellElement {
+	let cell = document.createElement("th");
+	cell.innerText = text;
+	return cell;
+}
+
+function setupInfoTable(): void {
+	let tbl = document.querySelector<HTMLTableElement>("[data-tab='info'] table");
+	
+
+	// set up header
+	for(let stat of statNames) {
+		// FIXME also need sorting buttons
+		tbl.tHead.rows[0].append(createTH(stat));
+	}
+	tbl.tHead.rows[0].append(createTH("Color 1"));
+	tbl.tHead.rows[0].append(createTH("Color 2"));
+	tbl.tHead.rows[0].append(createTH("Owned"));
+
+	// rows for each pokemon
+	for(let pokemon of pkmnList) {
+		for(let tier in pokemon.grades) {
+			let grade = pokemon.grades[tier];
+			let row = tbl.tBodies[0].insertRow();
+
+			// name & grade
+			let cell = row.insertCell();
+			cell.innerText = pokemon.name;
+			cell.classList.add(tierNames[tier]);
+
+			// stats
+			for(let stat of statNames) {
+				cell = row.insertCell();
+				let val = getStatValue(stat, grade);
+				if(val) {
+					let dec = 0;
+					if(stat.includes("Attack") || stat.includes("Critical")) {
+						dec = 1;
+					}
+					cell.innerText = val.toFixed(dec);
+					cell.classList.add("stat");
+					cell.classList.add(val > 0 ? "good" : "bad");
+				}
+			}
+
+			// colors
+			row.insertCell().innerText = pokemon.colors[0];
+			cell = row.insertCell();
+			if(pokemon.colors.length > 1) cell.innerText = pokemon.colors[1];
+
+			// owned
+			row.insertCell();
+		}
+	}
+}
+
+function setup(): void {
+	// need falsy rather than nullsy
+	setActiveTab((document.location.hash || "#free").substring(1), false);
+
+	let input = document.querySelector<HTMLTextAreaElement>(
+		"[data-tab='free'] textarea"
+	);
 	input.addEventListener("keydown", () => createFromTextfield(input));
 	input.addEventListener("keyup", () => createFromTextfield(input));
 	input.addEventListener("paste", () => createFromTextfield(input));
+
+	document.querySelectorAll<HTMLElement>(".tab-list > *").forEach(
+		t => t.addEventListener("click", ev => setActiveTab(t.dataset.tab, true))
+	);
 
 	fetch("emblems.json").then(r => r.json()).then(json => {
 		pkmnList = json;
 		for(let pkmn of pkmnList) {
 			pkmnByName.set(pkmn.name, pkmn);
 		}
+
+		setupInfoTable();
 
 		createFromTextfield(input);
 	});
